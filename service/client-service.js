e4d3f53a989aedb2c934a20532867acd6f63e9a6
Martin Fernandez <fernandezmartin1534@gmail.com>
2023-01-27 13:47:54 -0300

    Reparando codigo, agregando la funcion eliminar cliente al programa

--- service/client-service.js
+++ service/client-service.js
@@ -1,25 +1,24 @@
-//Abrir http (método,url)
-
-// CRUD   - Métodos HTTP
-// Create - POST
-// Read   - GET
-// Update - PUT/PATCH
-// Delete - DELETE
-
-//Fetch API
-const listaClientes = () => fetch("http://localhost:3000/perfil").then((response) => response.json());
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 const crearCliente = (nombre, email) => {
   return fetch("http://localhost:3000/perfil", {
     method: "POST",
     headers: {
-      "content-type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({nombre, email, id:uuid.v4()})
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
   });
-}
+};
+
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
 
 export const clientServices = {
   listaClientes,
   crearCliente,
+  eliminarCliente,
 };
