58ba21a215fa748c39de08d358bf17b222628f40
Martin Fernandez <fernandezmartin1534@gmail.com>
2023-01-26 21:02:16 -0300

    Agregando clientes por id

--- service/client-service.js
+++ service/client-service.js
@@ -9,6 +9,17 @@
 //Fetch API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((response) => response.json());
 
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({nombre, email, id:uuid.v4()})
+  });
+}
+
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
